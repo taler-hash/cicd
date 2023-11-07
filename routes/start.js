068c9b14ec465699f9d8703d19cf1dc26d0269c7
DESKTOP-VAFMQVF\Tylier <juriepedrogas@gmail.com>
2023-11-07 15:47:58 +0800

    remove spaces

--- routes/start.js
+++ routes/start.js
@@ -2,8 +2,8 @@ const Router = require('koa-tree-router')
 
 const router = new Router()
 
-    router.get('/user',(ctx)=>{
-                ctx.body = 'user 1'
+router.get('/user',(ctx)=>{
+    ctx.body = 'user 1'
 })
 
 module.exports = router
